6c97b99906decc1c0f444af43dd588531c9db60b
Islam Sulaiman <ma.eslam@yahoo.com>
2023-06-13 19:24:03 +0300

    create client folder

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-import User from './users';
-import Favorites from './favorites';
-
-export const models = {
-  User,
-  Favorites,
-};
--- src/routes/search.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { searchMethods } from '../middelwares/search';
-import { errorHandling } from '../middelwares/errorFunction';
-
-const router = Router();
-
-router.get('/', errorHandling(searchMethods.serachData));
-
-export const searchRoute: Router = router;
