d5b920422867036f31285d5a374f9b574147809f
Islam Sulaiman <ma.eslam@yahoo.com>
2023-06-13 19:53:45 +0300

    Implement main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+// import Footer from "./components/Footer/Footer";
+import NavCom from "./Components/Navbar/Navbar";  
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RegistrationForm from "./Pages/Registeration/Registration";
+import LoginForm from "./Pages/Login/Login";
+import Footer from "./Components/Footer/Footer";
+
+// import Login from "./pages/Login/Login";
+// import UserProfile from "./pages/UserProfile/UserProfile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+
+      <Router>
+        <NavCom />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/registration" element={<RegistrationForm />} />
+          {/* <Route path="/user/profile" element={<UserProfile />} /> */}
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </>
   );
 }
 
