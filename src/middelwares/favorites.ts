39e18521d1005ef857ad349287121ffd2f35c32d
Islam Sulaiman <ma.eslam@yahoo.com>
2023-06-13 16:48:24 +0300

    Update add favorite inside favorite middelware to get the favoritId automatically from the request body

--- src/middelwares/favorites.ts
+++ src/middelwares/favorites.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable max-len */
 import { Request, Response } from 'express';
 
@@ -5,13 +6,15 @@ import { favoriteControllers } from '../controllers/favorites';
 import { userControllers } from '../controllers/users';
 
 const add = async (req: Request, res: Response) : Promise<Response> => {
-  const { href, data, links } = req.body;
+  const {
+    href, data, links, _id,
+  } = req.body;
   const increment = 1;
 
   const { nasa_id } = data[0];
 
   const userId = '648859c255935ea38dc0a9ee';
-  const favoriteId = '648859fb55935ea38dc0a9f0';
+  const favoriteId = _id.$oid;
 
   // 1 Check first if the favorite alread there in the favorite document using nasa_id
   const isFavoriteExists = await favoriteControllers.findByNasaId(nasa_id);
@@ -23,24 +26,19 @@ const add = async (req: Request, res: Response) : Promise<Response> => {
     if (!userFavorites) throw new Error('21');
 
     // increment counter inside this specific favorite
-    const incrementFav = await favoriteControllers.increment(nasa_id);
+    await favoriteControllers.increment(nasa_id);
+    return res.sendStatus(200);
   }
 
   // 2 add to favorite table if not already there
-  const myFavorites = await favoriteControllers.add({
+  await favoriteControllers.add({
     href, data, links, increment,
   });
 
-  //   console.log(`myFavorites ${myFavorites}`);
-
-  console.log(`userId: ${userId} favoriteId ${favoriteId}`);
-
   // 3 add to user array of favorites
-  const userFavorites = await userControllers.addFavorite(userId, favoriteId);
+  await userControllers.addFavorite(userId, favoriteId);
 
-  console.log(`userFavorites ${userFavorites}`);
-
-  return res.status(200).send(myFavorites);
+  return res.sendStatus(200);
 };
 
 export const favoriteMiddelware = {
